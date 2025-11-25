825f026e8f6756dd4a6fe5fa272ed5c2fb5e222e
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-25 08:12:40 +0200

    Add port and listen

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const databaseConnection = require("./database/connect");
 const products = require("./routes/products");
-
+const PORT = process.env.PORT || 3500;
 async function startApp() {
   try {
     await databaseConnection();
     app.use(express.json());
     app.use("/products", products);
+    app.listen(PORT, () => {
+      console.log(`Started listening to port: ${PORT}`);
+    });
   } catch (error) {
     console.log("Failed to connect to db");
     process.exit(1);
