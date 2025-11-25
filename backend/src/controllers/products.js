4565d55e667d90ac649b0bcb94f24500aaebd873
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-25 07:52:43 +0200

    create all endpoints for products route

---
+++ backend/src/controllers/products.js
@@ -0,0 +1,15 @@
+const getAllProducts = async (req, res) => {
+  res.send("all products");
+};
+const createOneProduct = async (req, res) => {
+  res.send("new product");
+};
+const getOneProduct = async (req, res) => {
+  res.send("existing product");
+};
+const updateOneProduct = async (req, res) => {
+  res.send("updated product");
+};
+const deleteOneProduct = async (req, res) => {
+  res.send("deleted one product");
+};
