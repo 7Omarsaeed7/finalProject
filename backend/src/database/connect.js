0328150305aa572f0bfba6cc387a1ac7d098f627
Mohamed Hany <MohamedEgy246@gmail.com>
2025-11-25 08:00:26 +0200

    change import type

--- backend/src/database/connect.js
+++ backend/src/database/connect.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 async function connect() {
   return mongoose.connect(process.env.DB).then(() => {
     console.log("connectd to db...");
